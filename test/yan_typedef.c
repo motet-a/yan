ab2554c9d4c158d69b0e2ad47a318ccab5b27cc0
Antoine Motet <motet_a@epitech.eu>
2016-05-28 11:43:30 +0200

    Add test/yan_typedef.c

---
+++ test/yan_typedef.c
@@ -0,0 +1,17 @@
+/*
+** yan_typedef.c for  in /home/antoine
+**
+** Made by antoine
+** Login   <antoine@epitech.net>
+**
+** Started on  Sat May 28 11:33:54 2016 antoine
+** Last update Sat May 28 11:33:54 2016 antoine
+*/
+
+/*
+** yan typedef t_my_funny_type
+*/
+
+t_my_funny_type function(t_my_funny_type a)
+{
+}
