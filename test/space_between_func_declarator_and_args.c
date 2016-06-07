7403f0a2531308fec2fd250de3a604ca15e492fe
Antoine Motet <motet_a@epitech.eu>
2016-06-07 12:44:16 +0200

    Add tests for the spaces between function names and args

---
+++ test/space_between_func_declarator_and_args.c
@@ -0,0 +1,14 @@
+/*
+** space_between_func_declarator_and_args.c for  in /home/antoine
+**
+** Made by antoine
+** Login   <antoine@epitech.net>
+**
+** Started on  Tue Jun  7 12:14:16 2016 antoine
+** Last update Tue Jun  7 12:14:16 2016 antoine
+*/
+
+int     main ()
+{
+  return (0);
+}
---
+++ test/space_between_func_name_and_args.c
@@ -0,0 +1,14 @@
+/*
+** space_between_func_name_and_args.c for  in /home/antoine
+**
+** Made by antoine
+** Login   <antoine@epitech.net>
+**
+** Started on  Tue Jun  7 12:19:19 2016 antoine
+** Last update Tue Jun  7 12:19:19 2016 antoine
+*/
+
+int     main()
+{
+  some_function ();
+}
