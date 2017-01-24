a46f14de5a2bcd4ac48060debfb766f82adaff30
Antoine Motet <motet_a@epitech.eu>
2017-01-24 09:40:17 +0100

    Check space after sizeof

---
+++ test/sizeof_valid.c
@@ -0,0 +1,16 @@
+/*
+** sizeof_valid.c for  in /home/antoine
+**
+** Made by antoine
+** Login   <antoine@epitech.net>
+**
+** Started on  Tue Jan 24 09:25:14 2017 antoine
+** Last update Tue Jan 24 09:25:14 2017 antoine
+*/
+
+int             main()
+{
+  char          a;
+
+  return (sizeof(int) + sizeof a);
+}
